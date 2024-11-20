dd00c57d87934c3ad5b95272b4c5d11a9f16c518
jose cassio <josecassio2013@gmail.com>
2024-11-20 17:15:28 -0300

    feat: make wall view and camera position

---
+++ scripts/player/camera_rig.gd
@@ -0,0 +1,18 @@
+extends Node3D
+@onready var background_viewport_container = $base_camera/background_viewport_container/background_viewport
+@onready var forground_viewport_container = $base_camera/forground_viewport_container/forground_viewport
+@onready var background_camera = $base_camera/background_viewport_container/background_viewport/background_camera
+@onready var forground_camera = $base_camera/forground_viewport_container/forground_viewport/forground_camera
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	resize()
+
+func resize():
+	background_viewport_container.size = DisplayServer.window_get_size()
+	forground_viewport_container.size = DisplayServer.window_get_size()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	background_camera.global_transform = GameManager.player.camera_point.global_transform
+	forground_camera.global_transform = GameManager.player.camera_point.global_transform
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const SPEED = 3.0
 const JUMP_VELOCITY = 4.5
+
+@onready var camera_point = $camera_point
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -10,10 +12,13 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready  var animation_player = $visuals/player/AnimationPlayer
 @onready var visuals = $visuals
 
-
-
 var walking = false
 
+func _ready():
+	GameManager.set_player(self)
+	animation_player.set_blend_time("idle", "walk", 0.2)
+	animation_player.set_blend_time("walk", "idle", 0.2)
+	
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -31,8 +36,9 @@ func _physics_process(delta):
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 		
-		animation_player.play("run")
-		visuals.look_at(direction + position)
+		
+		animation_player.play("walk")
+		visuals.look_at(velocity + position, Vector3.UP)
 		
 		if !walking: 
 			walking = true
